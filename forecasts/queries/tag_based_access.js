fd7c2c1c12ab2e0c275031c374116a0e3fd8a06b
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-05-26 13:32:51 +0530

    New document and a query to demonstrate $redact

---
+++ forecasts/queries/tag_based_access.js
@@ -0,0 +1,32 @@
+/**
+ * Assume the user has access to the provided tags.
+ * Fetch all data accessible to him based on the access configurations
+ * Here's how the configurations work:
+ * 
+ * 1)   If the user has an access "X", then he can access all levels whose tags value 
+ *      includes "X"
+ * 2)   If the user doesn't have an access "X", then he cannot access any level
+ *      that needs access "X", and all deeper levels
+ */
+
+ let userAccess = ["T"];
+
+ db.forecasts.aggregate([
+     {$redact : {
+         $cond: {
+             if : {$gt : [{$size : {$setIntersection: [userAccess, "$tags"]}} ,0]},
+             then: "$$DESCEND",
+             else: "$$PRUNE"
+         }
+     }}
+ ]).pretty();
+
+ db.forecasts.aggregate([
+    {$redact : {
+        $cond: {
+            if : {$gt : [{$size : {$setIntersection: [userAccess, "$tags"]}} ,0]},
+            then: "$$KEEP",
+            else: "$$PRUNE"
+        }
+    }}
+]).pretty();
